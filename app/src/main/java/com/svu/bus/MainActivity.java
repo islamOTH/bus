8a537596c7dd04cfca58744d2043ee0b22e88568
islam <61045683+islamOTH@users.noreply.github.com>
2022-12-23 22:57:42 +0200

    Initial commit

---
+++ app/src/main/java/com/svu/bus/MainActivity.java
@@ -0,0 +1,33 @@
+package com.svu.bus;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/svu/bus/User.java
@@ -0,0 +1,75 @@
+package com.svu.bus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class User
+{
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String rule;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getRule() {
+        return rule;
+    }
+
+    public User setRule(String rule) {
+        this.rule = rule;
+        return this;
+    }
+
+    public Map<String , Object> toMap()
+    {
+        Map<String,Object> map=new HashMap<>();
+        map.put("id",id);
+        map.put("name",name);
+        map.put("email",email);
+        map.put("password",password);
+        map.put("rule",rule);
+        return map;
+    }
+    public static User fromMap(Map<String,Object> map)
+    {
+    return new User().setEmail(map.get("email").toString());
+    }
+
+}
