ca8482f09fc72e539136dc48cb67dd98e4a8510f
islam <61045683+islamOTH@users.noreply.github.com>
2022-12-24 13:01:11 +0200

    login register

---
+++ app/src/main/java/com/svu/bus/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.svu.bus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.svu.bus.databinding.ActivityLoginBinding;
+
+
+
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        binding.cirLoginButton.setIndeterminateProgressMode(true);
+        binding.cirLoginButton.setOnClickListener(v -> {
+
+            if (binding.cirLoginButton.getProgress() == 0) {
+                binding.cirLoginButton.setProgress(50);
+            } else if (binding.cirLoginButton.getProgress() == -1) {
+                binding.cirLoginButton.setProgress(0);
+            } else {
+                binding.cirLoginButton.setProgress(-1);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/svu/bus/RegisterActivity.java
@@ -0,0 +1,78 @@
+package com.svu.bus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.svu.bus.databinding.ActivityRegisterBinding;
+
+
+public class RegisterActivity extends AppCompatActivity {
+ActivityRegisterBinding binding;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_register);
+        binding.cirRegisterButton.setIndeterminateProgressMode(true);
+        binding.cirRegisterButton.setOnClickListener(v ->{if(binding.cirRegisterButton.getProgress()==0) register();});
+        binding.btnLogin.setOnClickListener(v ->{ startActivity(new Intent(this,LoginActivity.class));finish();});
+    }
+
+    private void register()
+    { if (binding.cirRegisterButton.getProgress() == 0) {
+        binding.cirRegisterButton.setProgress(50);
+        //binding.cirRegisterButton.setEnabled(false);
+    }
+
+        if (binding.editTextEmail.getText().toString().isEmpty() || binding.editTextMobile.getText().toString().isEmpty() || binding.editTextName.getText().toString().isEmpty() || binding.editTextPassword.getText().toString().isEmpty())
+    { binding.cirRegisterButton.setProgress(-1);
+        new Handler().postDelayed(() -> {binding.cirRegisterButton.setProgress(0);},3000);
+        Toast.makeText(this, "please enter all data and try again", Toast.LENGTH_SHORT).show();
+        return;
+    }
+        if (binding.editTextPassword.getText().toString().length()<8)
+        {binding.cirRegisterButton.setProgress(-1);
+            new Handler().postDelayed(() -> {binding.cirRegisterButton.setProgress(0);},3000);
+            Toast.makeText(this, "please password length > 8", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        new User()
+                .setEmail(binding.editTextEmail.getText().toString())
+                .setPassword(binding.editTextPassword.getText().toString())
+                .setRule("user")
+                .setName(binding.editTextName.getText().toString())
+                .setPhone(binding.editTextMobile.getText().toString())
+                .insert(new InsertAction() {
+                    @Override
+                    public void insertComplate(User user) {
+                            binding.cirRegisterButton.setProgress(100);
+new Handler().postDelayed(() -> {
+startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+finish();
+},3000);
+                        Toast.makeText(RegisterActivity.this, "success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void insertError(String s) {
+                        binding.cirRegisterButton.setProgress(-1);
+                        new Handler().postDelayed(() -> {
+                            binding.cirRegisterButton.setEnabled(true);
+                            binding.cirRegisterButton.setProgress(0);
+
+                        },3000);
+                        Toast.makeText(RegisterActivity.this, s, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+    }
+}
